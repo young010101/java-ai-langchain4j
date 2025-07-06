8fb21905ca8e07a9caa9b335c47896141139f382
jing_mac_air <3022004056@tju.edu.cn>
2025-07-06 16:56:48 +0800

    万象百炼deepseek

--- src/test/java/com/atguigu/java/ai/langchain4j/LLMTest.java
+++ src/test/java/com/atguigu/java/ai/langchain4j/LLMTest.java
@@ -2,7 +2,10 @@ package com.atguigu.java.ai.langchain4j;
 
 import ch.qos.logback.classic.spi.EventArgUtil;
 import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.community.model.dashscope.WanxImageModel;
+import dev.langchain4j.data.image.Image;
 import dev.langchain4j.model.openai.OpenAiChatModel;
+import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class LLMTest {
 
     @Test
-    public void testGPTDemo(){
+    public void testGPTDemo() {
 
         OpenAiChatModel model = OpenAiChatModel.builder()
                 .baseUrl("http://langchain4j.dev/demo/openai/v1")
@@ -28,7 +31,7 @@ public class LLMTest {
     private OpenAiChatModel OpenAiChatModel;
 
     @Test
-    public void testSpringBoot(){
+    public void testSpringBoot() {
         String answer = OpenAiChatModel.chat("你是谁？");
         System.out.println(answer);
     }
@@ -39,6 +42,7 @@ public class LLMTest {
 
     @Autowired
     private QwenChatModel qwenChatModel;
+
     @Test
     public void testDashScopeQwen() {
         //向模型提问
@@ -47,6 +51,15 @@ public class LLMTest {
         System.out.println(answer);
     }
 
+    @Test
+    public void testDashScopeWanx() {
+        WanxImageModel wanxImageModel = WanxImageModel.builder()
+                .modelName("wanx2.1-t2i-plus")
+                .apiKey(System.getenv("API_KEY"))
+                .build();
+        Response<Image> response = wanxImageModel.generate("奇幻森林精灵：在一片弥漫着轻柔薄雾的古老森林深处，阳光透过茂密枝叶洒下金色光斑。一位身材娇小、长着透明薄翼的精灵少女站在一朵硕大的蘑菇上。她有着海藻般的绿色长发，发间点缀着蓝色的小花，皮肤泛着珍珠般的微光。身上穿着由翠绿树叶和白色藤蔓编织而成的 连衣裙，手中捧着一颗散发着柔和光芒的水晶球，周围环绕着五彩斑斓的蝴蝶，脚下是铺满苔藓的地面，蘑菇和蕨类植 物丛生，营造出神秘而梦幻的氛围。 ");
+        System.out.println(response.content().url());
+    }
 
 
 }
