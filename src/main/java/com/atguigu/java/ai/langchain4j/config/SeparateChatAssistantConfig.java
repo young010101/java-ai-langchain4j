194726b0e718d2abf24c14ff153a88fac0b0118a
jing_mac_air <3022004056@tju.edu.cn>
2025-07-08 22:34:28 +0800

    Add MongoDB integration and chat memory store

---
+++ src/main/java/com/atguigu/java/ai/langchain4j/config/SeparateChatAssistantConfig.java
@@ -0,0 +1,29 @@
+package com.atguigu.java.ai.langchain4j.config;
+
+import com.atguigu.java.ai.langchain4j.store.MongoChatMemoryStore;
+import dev.langchain4j.memory.ChatMemory;
+import dev.langchain4j.memory.chat.ChatMemoryProvider;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.store.memory.chat.InMemoryChatMemoryStore;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeparateChatAssistantConfig {
+
+    @Autowired
+    private MongoChatMemoryStore mongoChatMemoryStore;
+
+    @Bean
+    public ChatMemoryProvider chatMemoryProvider() {
+
+        return memoryId -> MessageWindowChatMemory
+                .builder()
+                .id(memoryId)
+                .maxMessages(10)
+                //.chatMemoryStore(new InMemoryChatMemoryStore())
+                .chatMemoryStore(mongoChatMemoryStore)
+                .build();
+    }
+}
