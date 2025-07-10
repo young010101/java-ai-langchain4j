db8aa19a3c4cf6ee8f60ac91ec8d4eeea1e817f8
jing_mac_air <3022004056@tju.edu.cn>
2025-07-10 22:19:17 +0800

    PromptTest--Usermessage提示词

--- src/test/java/com/atguigu/java/ai/langchain4j/PromptTest.java
+++ src/test/java/com/atguigu/java/ai/langchain4j/PromptTest.java
@@ -1,5 +1,6 @@
 package com.atguigu.java.ai.langchain4j;
 
+import com.atguigu.java.ai.langchain4j.assistant.MemoryChatAssistant;
 import com.atguigu.java.ai.langchain4j.assistant.SeparateChatAssistant;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,16 @@ public class PromptTest {
     @Test
     public void testSystemMessage() {
         String answer = separateChatAssistant.chat(3, "今天几号");
+        System.out.println(answer);
+    }
+
+
+    @Autowired
+    private MemoryChatAssistant memoryChatAssistant;
+
+    @Test
+    public void testUserMessage() {
+        String answer = memoryChatAssistant.chat("我是环环");
         System.out.println(answer);
     }
 }
