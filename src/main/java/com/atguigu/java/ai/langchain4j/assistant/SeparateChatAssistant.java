938a14d52b610d5d794fd090906fe49f046fa7e5
jing_mac_air <3022004056@tju.edu.cn>
2025-07-10 22:05:36 +0800

    PromptTest

--- src/main/java/com/atguigu/java/ai/langchain4j/assistant/SeparateChatAssistant.java
+++ src/main/java/com/atguigu/java/ai/langchain4j/assistant/SeparateChatAssistant.java
@@ -1,6 +1,7 @@
 package com.atguigu.java.ai.langchain4j.assistant;
 
 import dev.langchain4j.service.MemoryId;
+import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 import dev.langchain4j.service.spring.AiService;
 
@@ -13,7 +14,7 @@ import static dev.langchain4j.service.spring.AiServiceWiringMode.EXPLICIT;
         )
 
 public interface SeparateChatAssistant {
-
+    @SystemMessage("你是我的好朋友，请用东北话回答问题。今天是{{current_date}}")//系统消息提示词
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
 
 }
